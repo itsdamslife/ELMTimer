80b3a5a3878a0ce9e7596025517f9da7ffa3a305
Damodar Shenoy <dshenoy@vmware.com>
2019-05-14 18:08:46 +0530

    Structure and base for Elm arch

---
+++ TEA/TimerModel.swift
@@ -0,0 +1,77 @@
+//
+//  TimerModel.swift
+//  TEA
+//
+//  Created by Damodar Shenoy on 14/05/19.
+//  Copyright © 2019 It's Dams Life. All rights reserved.
+//
+
+import Foundation
+
+class TimerModel {
+    static let timerValueDidChange = Notification.Name("timerValueDidChange")
+    static let textKey = "timer"
+    
+    private var hours: Int = 0
+    private var mins: Int = 0
+    private var seconds: Int = 0
+
+    private var timer: Timer?
+    
+    private var value: String {
+        didSet {
+            NotificationCenter.default.post(name: TimerModel.timerValueDidChange, object: self, userInfo: [TimerModel.textKey: value])
+        }
+    }
+    
+    init() {
+        self.value = "00:00:00"
+    }
+    
+    
+    private func incrCountDownTime() {
+        seconds += 1
+        if seconds > 59 {
+            mins += 1
+            seconds = 0
+        }
+        if mins > 59 {
+            hours += 1
+            mins = 0
+        }
+        updateValue()
+    }
+    
+    private func reset() {
+        hours = 0
+        mins = 0
+        seconds = 0
+        updateValue()
+    }
+    
+    private func updateValue() {
+        var displayStr = (hours < 10) ? "0\(hours)" : "\(hours)"
+        displayStr += (mins < 10) ? ":0\(mins)" : ":\(mins)"
+        displayStr += (seconds < 10) ? ":0\(seconds)" : ":\(seconds)"
+        value = displayStr
+    }
+    
+    func startTimer() {
+        if self.timer != nil {
+            self.timer?.invalidate()
+            self.timer = nil
+        }
+        
+        // reset first
+        reset()
+        value = "00:00:00"
+
+        self.timer = Timer.scheduledTimer(withTimeInterval: 1.0, repeats: true, block: { [unowned self] timer in
+            self.incrCountDownTime()
+        })
+    }
+    
+    func stopTimer() {
+        self.timer?.invalidate()
+    }
+}
--- TEA/ViewController.swift
+++ TEA/ViewController.swift
@@ -15,26 +15,6 @@ struct CountDownTimer {
     var hours: Int = 0
     var mins: Int = 0
     var seconds: Int = 0
-    
-    mutating func incrCountDownTime() {
-        seconds += 1
-        if seconds > 59 {
-            mins += 1
-            seconds = 0
-        }
-        if mins > 59 {
-            hours += 1
-            mins = 0
-        }
-    }
-    
-    mutating func reset() {
-        hours = 0
-        mins = 0
-        seconds = 0
-    }
-    
-    // Ambiguity : Display string formatting to be given from here or from VC?
 }
 
 extension String.StringInterpolation {
@@ -63,11 +43,15 @@ class ViewController: UIViewController {
             self.timer?.invalidate()
             self.timer = nil
         }
-        
+
+        // reset first
+        self.reset()
+        self.timerLabel.text = "00:00:00"
+
         // Ambiguity : Who is responsible for timer?
         self.timer = Timer.scheduledTimer(withTimeInterval: 1.0, repeats: true, block: { [unowned self] timer in
             
-            self.timerModel.incrCountDownTime()
+            self.incrCountDownTime()
 //            print("The current time elapsed is : \(String(describing: self.timerModel))")
             
             // Update label
@@ -82,8 +66,24 @@ class ViewController: UIViewController {
     
     @IBAction func stop(_ sender: UIButton) {
         self.timer?.invalidate()
-        self.timerModel.reset()
-        self.timerLabel.text = "00:00:00"
+    }
+    
+    func incrCountDownTime() {
+        timerModel.seconds += 1
+        if timerModel.seconds > 59 {
+            timerModel.mins += 1
+            timerModel.seconds = 0
+        }
+        if timerModel.mins > 59 {
+            timerModel.hours += 1
+            timerModel.mins = 0
+        }
+    }
+    
+    func reset() {
+        timerModel.hours = 0
+        timerModel.mins = 0
+        timerModel.seconds = 0
     }
 }
 
